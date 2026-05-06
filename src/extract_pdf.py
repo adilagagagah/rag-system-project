37880164191d724314ef8210680adb118f47e7ec
Gagah Kantor <gagahpusokoadilaga03@gmail.com>
2026-05-06 10:47:44 +0700

    update data, gitignore, structure; add requirements.txt;

--- src/extract_pdf.py
+++ src/extract_pdf.py
@@ -1,6 +1,6 @@
 #! pip install pymupdf
 import fitz  
 
-doc = fitz.open("data/Executive_Report_DWS_October_2025.pdf")
+doc = fitz.open("data/raw/Skripsi Cetak_Gagah Pusoko Adilaga.pdf")
 print(doc.page_count)
 
